e325d8818ef373c4d98c527b0320af21883f00fe
Ra1nJun <Over4ction@gmail.com>
2025-04-23 22:07:49 +0900

    student class

---
+++ src/workshop/account/test/AccountTest.java
@@ -0,0 +1,9 @@
+package workshop.account.test;
+
+import workshop.account.entity.Account;
+
+public class AccountTest {
+	public static void main(String[] args) {
+		Account account = new Account();
+	}
+}
